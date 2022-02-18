1c83cee359f43fca0357c7e9aac7fa2b7b37e811
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-02-18 11:58:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
         res.send(`Hi i am nodejs deploy me on k8s from ${os.hostname()}!`)
 })
 
-const port = 3000
+const port = 3009
 app.listen(port, () => console.log(`listening on port ${port}`))
